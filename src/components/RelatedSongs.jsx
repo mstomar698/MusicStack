b9c1ce3c10df9153c051c998de88a03be5b262e5
mstomar698 <tomarm698@gmail.com>
2023-02-02 01:14:33 +0530

    setting RelatedSongs  Component

--- src/components/RelatedSongs.jsx
+++ src/components/RelatedSongs.jsx
@@ -1,5 +1,30 @@
-const RelatedSongs = () => (
-  <div>Loader</div>
-);
+import SongBar from './SongBar';
+
+const RelatedSongs = ({
+  data,
+  artistId,
+  isPlaying,
+  activeSong,
+  handlePauseClick,
+  hanldePlayClick,
+}) => {
+  <div className="flex flex-col">
+    <h1 className="font-bol text-3xl text-white">Related Songs:</h1>
+
+    <div className="mt-6 w-full flex flex-col">
+      {data?.map((song, i) => (
+        <SongBar
+          key={`${artistId}-${song.key}-${i}`}
+          song={song}
+          artistId={artistId}
+          isPlaying={isPlaying}
+          activeSong={activeSong}
+          handlePauseClick={handlePauseClick}
+          handlePlayClick={hanldePlayClick}
+        />
+      ))}
+    </div>
+  </div>;
+};
 
 export default RelatedSongs;
