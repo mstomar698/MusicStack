1ce6e695b5cffcf3e0a5e10ad42203cd63757c57
mstomar698 <tomarm698@gmail.com>
2023-02-02 01:08:17 +0530

    setting PlayPause  Component

--- src/components/PlayPause.jsx
+++ src/components/PlayPause.jsx
@@ -1,6 +1,8 @@
 import { FaPauseCircle, FaPlayCircle } from 'react-icons/fa';
 
+// eslint-disable-next-line no-confusing-arrow
 const PlayPause = ({ isPlaying, activeSong, song, handlePause, handlePlay }) =>
+  // eslint-disable-next-line implicit-arrow-linebreak
   isPlaying && activeSong?.title === song.title ? (
     <FaPauseCircle size={35} className="text-gray-300" onClick={handlePause} />
   ) : (
