55da84ff8eb70a8f4e6405a248e38e418a932286
mstomar698 <tomarm698@gmail.com>
2023-02-02 01:06:51 +0530

    setting Error  Component

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,9 +1,7 @@
-import React from 'react';
-
 const Error = () => (
   <div className="w-full flex justify-center items-center">
     <h1 className="font-bold text-2xl text-white mt-2">
-      You gotta Error Comming!!
+      Something ent Wrong...
     </h1>
   </div>
 );
