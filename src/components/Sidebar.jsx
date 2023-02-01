f1b105f78a0a6a88cf3e74ad84c7933ecdc44e50
mstomar698 <tomarm698@gmail.com>
2023-02-02 01:25:54 +0530

    setting Sidebar Component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { RiCloseLine } from 'react-icons/ri';
-import {HiOutlineMenu } from 'react-icons/hi';
+import { HiOutlineMenu } from 'react-icons/hi';
 
 import { logo } from '../assets';
 import { links } from '../assets/constants';
@@ -32,11 +32,25 @@ const Sidebar = () => {
         <NavLinks />
       </div>
       <div className="absolute md:hidden block top-6 right-3">
-        {mobileMenuOpen ? (<RiCloseLine className="w-6 h-6 text-white mr-2" onClick={() => setMobileMenuOpen(false)}/>) : <HiOutlineMenu className="w-6 h-6 text-white mr-2" onClick={() => setMobileMenuOpen(true)}/>}
+        {!mobileMenuOpen ? (
+          <HiOutlineMenu
+            className="w-6 h-6 mr-2 text-white"
+            onClick={() => setMobileMenuOpen(true)}
+          />
+        ) : (
+          <RiCloseLine
+            className="w-6 h-6 mr-2 text-white"
+            onClick={() => setMobileMenuOpen(false)}
+          />
+        )}
       </div>
-      <div className={`absolute top-0 h-screen w-2/3 bg-gradient-tot1 from-white/10 to-[#483d8b] backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${mobileMenuOpen ? 'left-0' : '-left-full'}`}>
+      <div
+        className={`absolute top-0 h-screen w-2/3 bg-gradient-to-tl from-white/10 to-[#483D8B] backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${
+          mobileMenuOpen ? 'left-0' : '-left-full'
+        }`}
+      >
         <img src={logo} alt="logo" className="w-full h-14 object-contain" />
-        <NavLinks handleClick={() => setMobileMenuOpen(false)}/>
+        <NavLinks handleClick={() => setMobileMenuOpen(false)} />
       </div>
     </>
   );
