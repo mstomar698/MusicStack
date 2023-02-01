17afed9f6ca170bee21f9acbdcdc6c8f21868fd6
mstomar698 <tomarm698@gmail.com>
2023-02-02 00:07:52 +0530

    setting SongDetails page

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -1,3 +1,76 @@
-const SongDetails = () => <div>SongDetails</div>;
+import { useParams } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { DetailsHeader, Error, Loader, RelatedSongs } from '../components';
+
+import { setActiveSong, playPause } from '../redux/features/playerSlice';
+import {
+  useGetSongDetailsQuery,
+  useGetSongRelatedQuery,
+} from '../redux/services/shazamCore';
+
+const SongDetails = () => {
+  const dispatch = useDispatch();
+  const { songId, id: artistId } = useParams();
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+
+  const {
+    data,
+    isFetching: isFetchingRelatedSongs,
+    error,
+  } = useGetSongRelatedQuery({ songId });
+  // eslint-disable-next-line operator-linebreak
+  const { data: songData, isFetching: isFecthingSongDetails } =
+    useGetSongDetailsQuery({ songId });
+
+  if (isFetchingRelatedSongs && isFecthingSongDetails)
+    // eslint-disable-next-line nonblock-statement-body-position, curly
+    return <Loader title="Searching Song Details" />;
+  if (error) return <Error />;
+
+  const handlePauseClick = () => {
+    dispatch(playPause(false));
+  };
+
+  const handlePlayClick = (song, i) => {
+    dispatch(setActiveSong({ song, data, i }));
+    dispatch(playPause(true));
+  };
+
+  return (
+    <div className="flex flex-col">
+      <DetailsHeader artistId={artistId} songData={songData} />
+
+      <div className="mb-10">
+        <h2 className="text-white text-3xl font-bold">Lyrics:</h2>
+
+        <div className="mt-5">
+          {songData?.sections[1].type === 'LYRICS' ? (
+            songData?.sections[1]?.text.map((line, i) => (
+              <p
+                key={`lyrics-${line}-${i}`}
+                className="text-gray-400 text-base my-1"
+              >
+                {line}
+              </p>
+            ))
+          ) : (
+            <p className="text-gray-400 text-base my-1">
+              Sorry Lyrics not Found
+            </p>
+          )}
+        </div>
+      </div>
+
+      <RelatedSongs
+        data={data}
+        artistId={artistId}
+        isPlaying={isPlaying}
+        activeSong={activeSong}
+        handlePauseClick={handlePauseClick}
+        handlePlayClick={handlePlayClick}
+      />
+    </div>
+  );
+};
 
 export default SongDetails;
